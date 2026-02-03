de36270bb0588453c019d0d8d60f5723da20d97f
PAUSE <Callumdavid4664@gmail.com>
2026-02-03 12:00:05 +0000

    Added Navigation and two new pages 😃

---
+++ main.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+    const menu = document.querySelector('.nav-links');
+    const burger = document.querySelector('.burger');
+    
+    menu.classList.toggle('open');
+    burger.classList.toggle('open');
+}
